82a6a1876c5fb03fb9b787ac8cd09742dd386dbf
Megha Gopalakrishnan <am.megha.gr@gmail.com>
2025-12-21 12:43:39 +0530

    CSS updates

--- src/component.jsx/List.jsx
+++ src/component.jsx/List.jsx
@@ -1,9 +1,9 @@
 
 import Todoitem from "./Todoitem"
-import style from "./list.module.css"
+
 export default function List({todos}) {
   return(
-  <div className={style.list}>
+  <div>
      {todos.map((item,index)=>(
         <Todoitem key={index} item={item}/>
         ))}
